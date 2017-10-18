20bb52403ed4b418b75ef1e628f9aad695319d2c
Visatouch Deeying <xerodotc@gmail.com>
2017-10-18 14:53:34 +0700

    More detailed descriptions

--- example.py
+++ example.py
@@ -7,6 +7,6 @@ def locationChangeCallback(addr, location, piece, speed, clockwise):
 
 car = Overdrive("xx:xx:xx:xx:xx:xx")
 car.setLocationChangeCallback(locationChangeCallback) # Set location change callback to function above
-car.changeSpeed(500, 1000) # Set car speed
-car.changeLaneRight(1000, 1000) # Switch to next right lane
+car.changeSpeed(500, 1000) # Set car speed with speed = 500, acceleration = 1000
+car.changeLaneRight(1000, 1000) # Switch to next right lane with speed = 1000, acceleration = 1000
 input() # Hold the program so it won't end abruptly
